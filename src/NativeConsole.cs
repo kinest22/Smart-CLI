aba12a63b8e421582c7d4030ea65be17e9f2b520
kinest22 <nirvana897@gmail.com>
2023-09-20 01:54:44 +0300

    Add project files.

---
+++ src/NativeConsole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SmartCLI
+{
+    /// <summary>
+    ///     Wrapper over native console operations.
+    /// </summary>
+    /// <remarks>
+    ///     WARNING: unsafe code here. Internal usage only.
+    /// </remarks>
+    internal class NativeConsole
+    {
+
+    }
+}
