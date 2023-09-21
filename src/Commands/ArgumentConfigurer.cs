aeac578a46217d5ac8da9f03aa7d00d0f84f0d03
kinest22 <nirvana897@gmail.com>
2023-09-22 01:41:05 +0300

    ArgumentConfigurer and NumericArgumentConfigurer class added to scope.

---
+++ src/Commands/ArgumentConfigurer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace SmartCLI.Commands
+{
+    /// <summary>
+    ///     Fluent configurer for argument.
+    /// </summary>
+    /// <typeparam name="TConfigurer">Configurer type.</typeparam>
+    /// <typeparam name="TArg">Argument type.</typeparam>
+    public class ArgumentConfigurer<TConfigurer, TArg> 
+        where TConfigurer : ArgumentConfigurer<TConfigurer, TArg>
+        where TArg : Argument
+    {
+        private protected readonly TArg _arg;
+        private protected TConfigurer _configurer = null!;
+
+        public ArgumentConfigurer(TArg arg)
+            => _arg = arg;
+
+        /// <summary>
+        ///     Sets the name for the argument.
+        /// </summary>
+        /// <returns><see cref="TConfigurer"/></returns>
+        public TConfigurer WithName(string name)
+        {
+            _arg.Name = name;
+            return _configurer;
+        }
+
+        /// <summary>
+        ///     Sets description for the argument.
+        /// </summary>
+        /// <returns><see cref="TConfigurer"/></returns>
+        public TConfigurer WithDescription(string description)
+        {
+            _arg.Description = description;
+            return _configurer;
+        }
+
+        /// <summary>
+        ///     Sets position for the argument in command-line input.
+        /// </summary>
+        /// <returns><see cref="TConfigurer"/></returns>
+        public TConfigurer WithPosition(int position)
+        {
+            _arg.Position = position;
+            return _configurer;
+        }
+    }
+}
---
+++ src/Commands/NumericArgumentConfigurer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCLI.Commands
+{
+    /// <summary>
+    ///     Fluent configurer for numeric-typed argument.
+    /// </summary>
+    /// <typeparam name="TArg">Numeric argument type.</typeparam>
+    public class NumericArgumentConfigurer<TArg> : ArgumentConfigurer<NumericArgumentConfigurer<TArg>, NumericArgument<TArg>>
+        where TArg : INumber<TArg>        
+    {
+        public NumericArgumentConfigurer() : base(new NumericArgument<TArg>())                 
+            => _configurer = this;
+
+        /// <summary>
+        ///     Sets argument's minimum value.
+        /// </summary>
+        /// <returns><see cref="NumericArgumentConfigurer{TArg}"/></returns>
+        public NumericArgumentConfigurer<TArg> WithMinValue(TArg minValue)
+        {
+            _arg.MinValue = minValue;
+            return this;
+        }
+
+        /// <summary>
+        ///     Sets argument's maximum value.
+        /// </summary>
+        /// <returns><see cref="NumericArgumentConfigurer{TArg}"/></returns>
+        public NumericArgumentConfigurer<TArg> WithMaxValue(TArg maxValue)
+        {
+            _arg.MaxValue = maxValue;
+            return this;
+        }
+
+        /// <summary>
+        ///     Sets argument's allowed values.
+        /// </summary>
+        /// <returns><see cref="NumericArgumentConfigurer{TArg}"/></returns>
+        public NumericArgumentConfigurer<TArg> WithAllowedValues(params TArg[] allowedValues)
+        {
+            _arg.AllowedValues = allowedValues;
+            return this;
+        }
+    }
+}
