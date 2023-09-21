82df2f677d18d88d44f081bc699465569e365fdb
kinest22 <nirvana897@gmail.com>
2023-09-21 18:08:33 +0300

    abstract arg + numeric arg classes added

---
+++ src/Commands/Argument.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace SmartCLI.Commands
+{
+    /// <summary>
+    ///     Represents argument that belongs to parameters of command.
+    /// </summary>
+    public abstract class Argument
+    {
+        /// <summary>
+        ///     Name of argument.
+        /// </summary>
+        public abstract string? Name { get; set; }
+
+        /// <summary>
+        ///     description of argument.
+        /// </summary>
+        public abstract string? Description { get; set; }
+
+        /// <summary>
+        ///     Position of argument in command line.
+        /// </summary>
+        public abstract int Position { get; set; }
+
+        /// <summary>
+        ///     Format used to parse value types arguments.
+        /// </summary>
+        public IFormatProvider? FormatProvider { get; set; }
+
+        /// <summary>
+        ///     Number style bitmask used to format numeric arguments.
+        /// </summary>
+        public NumberStyles? NumberStyle { get; set; }
+
+        /// <summary>
+        ///     In derived class parse string-represented argument from command line to specified type.
+        /// </summary>
+        /// <param name="strval"></param>
+        public abstract void Parse(string strval);
+    }
+}
---
+++ src/Commands/NumericArgument.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace SmartCLI.Commands
+{
+    public class NumericArgument<TArg> : Argument where TArg : INumber<TArg>
+    {
+        public override string? Name { get; set; }
+        public override string? Description { get; set; }
+        public override int Position { get; set; }
+        TArg? Value { get; set; }
+
+        public override void Parse(string strval)
+        {
+            var fmt = FormatProvider is null
+                ? CultureInfo.InvariantCulture
+                : FormatProvider;
+
+            var nstl = NumberStyle is null
+                ? NumberStyles.None
+                : NumberStyle.Value; 
+
+            Value = TArg.TryParse(strval, nstl, fmt, out TArg? parsed) is false
+                ? throw new FormatException($"Cannot parse '{strval}' as {typeof(TArg).Name}.")
+                : parsed;
+        }
+    }
+}
