cc82f82627227588bb3842116d498159180468e3
kinest22 <nirvana897@gmail.com>
2023-09-25 18:08:37 +0300

    string arg reworked. Regex support added

--- src/Commands/StringArgument.cs
+++ src/Commands/StringArgument.cs
@@ -1,14 +1,80 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace SmartCLI.Commands
 {
-    public class StringArgument : CollectionArgument<char>
+    /// <summary>
+    ///     Represents string-typed argument for command parameters
+    /// </summary>
+    public class StringArgument : Argument
     {
-        public StringArgument(Action<ICollection<char>> valueProvider) : base(valueProvider)
+        public StringArgument(Action<string> valueProvider) : base(valueProvider)
         {            
         }
 
+        /// <summary>
+        ///     Argument name.
+        /// </summary>
+        public override string? Name { get; set; }
+
+        /// <summary>
+        ///     Argument description.
+        /// </summary>
+        public override string? Description { get; set; }
+        
+        /// <summary>
+        ///     Argument position in command-line.
+        /// </summary>
+        public override int Position { get; set; }
+
+        /// <summary>
+        ///     Argumnet value;
+        /// </summary>
+        public string? Value { get; set; }
+
+        /// <summary>
+        ///     Argument length max value.
+        /// </summary>
+        public int? MaxLength { get; set; }
+
+        /// <summary>
+        ///     Regular expression pattern. Used to validate passed argument value for specific predefined pattern.
+        /// </summary>
+        public string? Pattern { get; set; }
+
+        /// <summary>
+        ///     Regular expression options used to find mathces for the <see cref="Value"/>. Used in pair with <see cref="Pattern"/>.
+        /// </summary>
+        public RegexOptions? RegExOptions { get; set; }
+
+        /// <summary>
+        ///     Parses <see cref="Value"/> from specified string 
+        /// </summary>
+        internal override void Parse(string strval)        
+            => Value = strval;
+
+        /// <summary>
+        ///     Provides argument value to command parameters.
+        /// </summary>
+        internal override void ProvideValue()        
+            => ((Action<string>)_valueProvider).Invoke(Value!);
+
+        /// <summary>
+        ///     Validates parsed argument value for max length.
+        /// </summary>
+        internal override void Validate()
+        {
+            if (MaxLength is not null && Value!.Length > MaxLength)
+                throw new ArgumentException($"{Value.Length} exceeds max length of {MaxLength}");
+
+            RegexOptions regopt = RegExOptions is null
+                ? RegexOptions.None
+                : RegExOptions.Value;
+
+            if (Pattern is not null && Regex.IsMatch(Value!, Pattern, regopt) is false)
+                throw new ArgumentException($"Passed value {Value} does not math regular expression {Pattern}");
+        }
     }
 }
--- src/Commands/StringArgumentConfigurer.cs
+++ src/Commands/StringArgumentConfigurer.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace SmartCLI.Commands
 {
+    /// <summary>
+    ///     Fluent configurer for string-typed argument.
+    /// </summary>
     public class StringArgumentConfigurer : ArgumentConfigurer<StringArgumentConfigurer, StringArgument>
     {
-        public StringArgumentConfigurer(Action<string> valueProvider) : base(new StringArgument((Action<ICollection<char>>)valueProvider))
-            => _configurer = this; // explicit cast is ok??
+        public StringArgumentConfigurer(Action<string> valueProvider) : base(new StringArgument(valueProvider))
+            => _configurer = this;
 
-        public StringArgumentConfigurer WithMinLength(int length)
+        /// <summary>
+        ///     Sets max length for argument value;
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public StringArgumentConfigurer WithMaxLength(int length)
         {
-            
+            _arg.MaxLength = length;
+            return this;
+        }
+
+        /// <summary>
+        ///     Sets regular expression for value to match.
+        /// </summary>
+        /// <param name="pattern">Regex pattern.</param>
+        /// <param name="regexOptions">Regex options.</param>
+        /// <returns></returns>
+        public StringArgumentConfigurer WithRegex(string pattern, RegexOptions? regexOptions)
+        {
+            _arg.Pattern = pattern;
+            _arg.RegExOptions = regexOptions;
             return this;
         }
     }
