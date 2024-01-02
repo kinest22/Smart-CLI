7a9b9cd58c7360251d39c59f6f44cad01e3cf59f
kinest22 <nirvana897@gmail.com>
2024-01-03 02:36:41 +0300

    bool parameter class added

---
+++ src/Commands/BoolParameter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SmartCLI.Commands
+{
+    public class BoolParameter : CommandParameter
+    {
+        public BoolParameter(Action<bool> valueProvider) : base(valueProvider)
+        {
+        }
+
+        public override string? Name { get; internal set; }
+
+        public override string? Description { get; set; }
+
+        public override int Position { get; internal set; }
+
+        public bool Value { get; set; }
+
+        internal override void Parse(string strval)
+        {
+            Value = bool.TryParse(strval, out bool parsed) is false
+                ? throw new FormatException($"Cannot parse '{strval}' as {typeof(bool).Name}.")
+                : parsed;
+        }
+
+        internal override void ProvideValue()
+            => ((Action<bool>)_valueProvider).Invoke(Value!);
+
+        internal override void ResetValue()
+            => ((Action<bool>)_valueProvider).Invoke(default!);
+
+        internal override void Validate()
+        {            
+        }
+    }
+}
