baedebfe203b15327f35fbd6db359a1f8c6eb1ae
kinest22 <nirvana897@gmail.com>
2024-01-06 16:49:49 +0300

    enum parameter and enum option configurer added to scope

---
+++ src/Commands/EnumOptionConfigurer.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace SmartCLI.Commands
+{
+    public class EnumOptionConfigurer<TEnum> : OptionConfigurer<EnumOptionConfigurer<TEnum>, EnumParameter<TEnum>>
+        where TEnum : struct, Enum
+    {
+        public EnumOptionConfigurer(string name, Action<TEnum> valueProvider) : base(name, new EnumParameter<TEnum>(valueProvider))
+            => _configurer = this;
+
+        /// <summary>
+        ///     Forces to consider case when parsing string value.
+        /// </summary>
+        /// <returns><see cref="EnumOptionConfigurer{TEnum}"/></returns>
+        public EnumOptionConfigurer<TEnum> ConsiderCaseForParsing()
+        {
+            _param.IgnoreCase = false;
+            return this;
+        }
+    }
+}
---
+++ src/Commands/EnumParameter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SmartCLI.Commands
+{
+    public class EnumParameter<TEnum> : CommandParameter where TEnum : struct, Enum
+    {
+        public EnumParameter(Action<TEnum> valueProvider) : base(valueProvider)
+        {
+        }
+
+        /// <summary>
+        ///     Name of parameter.
+        /// </summary>
+        public override string? Name { get; internal set; }
+
+        /// <summary>
+        ///     Parameter alias (for option case only)
+        /// </summary>
+        public override string? Alias { get; internal set; }
+
+        /// <summary>
+        ///     Description of parameter.
+        /// </summary>
+        public override string? Description { get; set; }
+
+        /// <summary>
+        ///     Position of parameter in command line.
+        /// </summary>
+        public override int Position { get; internal set; }
+
+        /// <summary>
+        ///     Ignore case flag for enum parameters. True to ignore case, false to consider case.
+        /// </summary>
+        public bool IgnoreCase { get; internal set; } = true;
+
+        /// <summary>
+        ///     Parameter value. Is subject to validation for min, max or allowed values constraints (if any).
+        /// </summary>
+        public TEnum? Value { get; set; }
+
+        /// <summary>
+        ///     Parses <see cref="Value"/> from specified string.
+        /// </summary>
+        /// <exception cref="FormatException"></exception>
+        internal override void Parse(string strval)
+        { 
+            Value = Enum.TryParse<TEnum>(strval, IgnoreCase, out var parsed) is false
+                ? throw new FormatException($"Cannot parse '{strval}' as {typeof(TEnum).Name}.")
+                : parsed;
+        }
+
+        /// <summary>
+        ///     Provides parameter value to command parameters.
+        /// </summary>
+        internal override void ProvideValue()
+            => ((Action<TEnum>)_valueProvider).Invoke(Value!.Value);
+
+
+        /// <summary>
+        ///     Resets parameter value.
+        /// </summary>
+        internal override void ResetValue()
+            => ((Action<TEnum>)_valueProvider).Invoke(default!);
+
+        /// <summary>
+        ///     Validates parsed parameter value for constraints.
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        internal override void Validate()
+        {            
+        }
+    }
+}
