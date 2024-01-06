39b55d3b5790746e7901859133055040182aef62
kinest22 <nirvana897@gmail.com>
2024-01-06 16:13:53 +0300

    Options configurer added

---
+++ src/Commands/OptionConfigurer.cs
@@ -0,0 +1,18 @@
+﻿
+namespace SmartCLI.Commands
+{
+    public abstract class OptionConfigurer<TConfigurer, TOpt> : CommandParameterConfigurer<TConfigurer, TOpt>
+        where TConfigurer : OptionConfigurer<TConfigurer, TOpt>
+        where TOpt : CommandParameter
+    {
+        public OptionConfigurer(string name, TOpt opt) : base(opt)         
+            => _param.Name = $"--{name.ToLower()}";
+        
+
+        public TConfigurer WithAlias(string alias)
+        {
+            _param.Alias = alias;
+            return _configurer;
+        }
+    }
+}
