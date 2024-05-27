2b2ef6790f7ca92727eeb82d8f8298b30d3d9a32
kinest22 <nirvana897@gmail.com>
2024-05-28 00:01:58 +0300

    NumericOptionConfigurer changed

--- src/Commands/NumericOptionConfigurer.cs
+++ src/Commands/NumericOptionConfigurer.cs
@@ -8,56 +8,6 @@ namespace SmartCLI.Commands
         where TOpt : INumber<TOpt>
     {
         public NumericOptionConfigurer(string name, Action<TOpt> valueProvider) : base(name, new NumericParameter<TOpt>(valueProvider))
-            => _configurer = this;
-
-        /// <summary>
-        ///     Sets parameter's minimum value.
-        /// </summary>
-        /// <returns><see cref="NumericOptionConfigurer{TOpt}"/></returns>
-        public NumericOptionConfigurer<TOpt> WithMinValue(TOpt minValue)
-        {
-            _param.MinValue = new TOpt[1] { minValue };
-            return this;
-        }
-
-        /// <summary>
-        ///     Sets parameter's maximum value.
-        /// </summary>
-        /// <returns><see cref="NumericOptionConfigurer{TOpt}"/></returns>
-        public NumericOptionConfigurer<TOpt> WithMaxValue(TOpt maxValue)
-        {
-            _param.MaxValue = new TOpt[1] { maxValue };
-            return this;
-        }
-
-        /// <summary>
-        ///     Sets parameter's allowed values.
-        /// </summary>
-        /// <returns><see cref="NumericArgumentConfigurer{TOpt}"/></returns>
-        public NumericOptionConfigurer<TOpt> WithAllowedValues(params TOpt[] allowedValues)
-        {
-            _param.AllowedValues = allowedValues;
-            return this;
-        }
-
-        /// <summary>
-        ///     Sets format provider to be used for parsing value.
-        /// </summary>
-        /// <returns><see cref="NumericOptionConfigurer{TOpt}"/></returns>
-        public NumericOptionConfigurer<TOpt> WithFormatProvider(IFormatProvider formatProvider)
-        {
-            _param.FormatProvider = formatProvider;
-            return this;
-        }
-
-        /// <summary>
-        ///     Sets number style to be used for parsing value.
-        /// </summary>
-        /// <returns><see cref="NumericOptionConfigurer{TOpt}"/></returns>
-        public NumericOptionConfigurer<TOpt> WithNumberStyle(NumberStyles numberStyle)
-        {
-            _param.NumberStyle = numberStyle;
-            return this;
-        }
+            => _configurer = this;       
     }
 }
