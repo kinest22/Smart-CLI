c5cd3b6cc613952b2ef74442337d971286081830
kinest22 <nirvana897@gmail.com>
2024-06-11 16:03:49 +0300

    CliUnitSearchEngine updated

--- src/CliUnitSearchEngine.cs
+++ src/CliUnitSearchEngine.cs
@@ -1,55 +1,69 @@
 ﻿using System;
-using System.Collections;
+using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using SmartCLI.Commands;
 
+[assembly: InternalsVisibleTo("demo")]
 namespace SmartCLI
 {
     /// <summary>
-    ///     Provides methods for bidirectional search for CLI basic units.
+    ///     Represents .
     /// </summary>
     internal class CliUnitSearchEngine
     {
         private static readonly CliUnitSearchEngine _engine = new();
         private readonly Dictionary<int, CliUnitTrie> _tries;
-        private readonly List<ISearchableUnit> _unitsFound;
+        private readonly List<ISearchableUnit> _res;
         private CliUnitTrie? _currentTrie;
-        private int _index;
 
         static CliUnitSearchEngine()
         {
         }
 
-        private CliUnitSearchEngine()
+        /// <summary>
+        ///     Creates new instance of <see cref="CliUnitSearchEngine"/>.
+        /// </summary>
+        internal CliUnitSearchEngine()
         {
             _tries = new();
-            _unitsFound = new();
+            _res = new();
         }
 
-        public static CliUnitSearchEngine Create()
+        /// <summary>
+        ///     Gets list of found CLI units.
+        /// </summary>
+        public IReadOnlyList<ISearchableUnit> SearchResult 
+            => _res;
+
+        /// <summary>
+        ///     Creates singleton instance of <see cref="CliUnitSearchEngine"/>.
+        /// </summary>
+        /// <returns>Instance of <see cref="CliUnitSearchEngine"/>.</returns>
+        internal static CliUnitSearchEngine Create()
             => _engine;
 
-
-
-        internal ISearchableUnit? GetNext(string wildcard, IReadOnlyList<ISearchableUnit> units)
+        /// <summary>
+        ///     Performs search by specified wildcard (prefix) within specified collection of CLI unis.
+        /// </summary>
+        /// <param name="wildcard">Wildcard (prefix)</param>
+        /// <param name="collection">Collection of CLI unis</param>
+        /// <returns>Number of CLI units found.</returns>
+        internal int FindByWildcard(string wildcard, IEnumerable<ISearchableUnit> collection)
         {
-
-            return null;
+            int len = 0;
+            int hash = collection.GetHashCode();
+            if (TryFindTrie(wildcard, hash, out _currentTrie))
+            {
+                _res.Clear();
+                len = FetchTerminalUnits(_currentTrie!, in _res);
+            }
+            return len;
         }
 
-
-        internal static ISearchableUnit? GetPrevious(string wildcard, IReadOnlyList<ISearchableUnit> units)
-        {
-
-            return null;
-        }
-
-
-
-
-
-
-
+        /// <summary>
+        ///     Registrates CLI unit collection and builds its <see cref="CliUnitTrie"/> for futher search.
+        /// </summary>
+        /// <param name="collection">CLI unit collection.</param>
         internal void RegisterUnitCollection(IEnumerable<ISearchableUnit> collection)
         {
             var hash = collection.GetHashCode();
@@ -63,40 +77,47 @@ namespace SmartCLI
             _tries.Add(hash, root);
         }
 
-
-        private bool TryFindSubTrie(string wildcard, IEnumerable<ISearchableUnit> collection)
+        /// <summary>
+        ///     Gets <see cref="CliUnitTrie"/> associated with specified wildcard (prefix) and CLI unit collection hash code.
+        /// </summary>
+        /// <param name="wildcard">Wildcard (prefix)</param>
+        /// <param name="hash">CLI unit collection hash code.</param>
+        /// <param name="trie"><see cref="CliUnitTrie"/> found.</param>
+        /// <returns>True if <see cref="CliUnitTrie"/> was found; otherwise false.</returns>
+        private bool TryFindTrie(string wildcard, int hash, out CliUnitTrie? trie)
         {
-            _currentTrie = null;
-            var hash = collection.GetHashCode();
-            if (_tries.TryGetValue(hash, out _currentTrie))
+            if (_tries.TryGetValue(hash, out trie))
             {
                 foreach (char c in wildcard)
                 {
-                    if (_currentTrie.SubTries.ContainsKey(c))
+                    if (trie.ContainsKey(c))
                     {
-                        _currentTrie = _currentTrie.SubTries[c];
+                        trie = trie[c];
                     }
                     else
                     {
-                        _currentTrie = null;
+                        trie = null;
                         return false;
                     }
                 }
+                return true;
             }
             return false;
         }
 
-        private int FetchTerminalUnits(CliUnitTrie trie)
+        /// <summary>
+        ///     Fetches all units from all terminal <see cref="CliUnitTrie"/>s of specified trie.
+        /// </summary>
+        /// <returns>Number of CLI unts found.</returns>
+        private int FetchTerminalUnits(CliUnitTrie trie, in List<ISearchableUnit> found)
         {
-            _unitsFound.Clear();
-
             if (trie.IsTerminal)
-                _unitsFound.Add(trie.Unit!);
+                found.Add(trie.Unit!);
 
-            foreach (char c in trie.SubTries.Keys)
-                FetchTerminalUnits(trie.SubTries[c]);
+            foreach (char c in trie.Keys)
+                FetchTerminalUnits(trie[c], in found);
 
-            return _unitsFound.Count;
+            return found.Count;
         }
     }
 }
