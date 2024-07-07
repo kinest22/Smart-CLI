e17978b35515ef39fc640771896905972a14b686
kinest22 <nirvana897@gmail.com>
2024-07-07 22:35:25 +0300

    comments added to CliStateMachine class

--- src/CliStateMachine.cs
+++ src/CliStateMachine.cs
@@ -6,6 +6,9 @@ using System.Text;
 
 namespace SmartCLI
 {
+    /// <summary>
+    ///     CLI Finite State Machine for CLI input processing.
+    /// </summary>
     internal sealed class CliStateMachine
     {
         /// <summary>
@@ -38,25 +41,25 @@ namespace SmartCLI
 #pragma warning restore IDE1006 // Naming Styles
 
 
-        private readonly CliUnitSearchEngine _searchEngine;
-        private readonly CliUnitSearchResults _unitsFound;
-        private readonly StringBuilder _buffer;
+        private readonly CliUnitSearchEngine _searchEngine;                     // CLI Search Engine used to search CLI units
+        private readonly CliUnitSearchResults _unitsFound;                      // cycled collection of CLI unts found
+        private readonly StringBuilder _buffer;                                 // input buffer
 
 
-        private IEnumerable<ICliUnit> _units;
-        private State _state;
+        private IEnumerable<ICliUnit> _units;                                   // current collection of unit available to search within
 
-        private int _tokenStartPosition;
-        
+        private State _state;                                                   // current machine state
+
+        private int _tokenStartPosition;                                        // pending token start position      
 
 
-        private ICliUnit? _unitGuess;
-        private ICliUnit? _spaceDefined;
-        private ICliUnit? _cmdDefined;
-        private ICliUnit? _optDefined;
+        private ICliUnit? _unitGuess;                                           // current CLI unit guess
+        private ICliUnit? _spaceDefined;                                        // command space already defined
+        private ICliUnit? _cmdDefined;                                          // command already defined
+        private ICliUnit? _optDefined;                                          // command option already defined
 
-        private string _wildcard;
-        private string _prompt;
+        private string _wildcard;                                               // current wildcard from buffer
+        private string _prompt;                                                 // current prompt according to wildcard 
 
 
         internal CliStateMachine(CliCommandHierarchy hierarchy)
@@ -71,7 +74,9 @@ namespace SmartCLI
         }
 
 
-
+        /// <summary>
+        ///     Resets state machine.
+        /// </summary>
         internal void Reset()
         {
             _unitsFound.Clear();
@@ -84,7 +89,10 @@ namespace SmartCLI
             _prompt = string.Empty;
         }
 
-
+        /// <summary>
+        ///     Processes specified input.
+        /// </summary>
+        /// <param name="cki"><see cref="ConsoleKeyInfo"/> entered.</param>
         internal void ProcessInput(ConsoleKeyInfo cki)
         {            
             var key = cki.Key;
@@ -128,12 +136,15 @@ namespace SmartCLI
                     break;
 
                 case (_, ALT):
+                    // ignore any ALT + smth inputs
                     break;
 
                 case (_, CTRL):
+                    // ignore any CTRL + smth inputs
                     break;
 
                 case (_, CTRL | ALT):
+                    // ignore any CTRL + ALT + smth inputs
                     break;
 
                 default:
@@ -143,16 +154,19 @@ namespace SmartCLI
             }
         }
 
-
-
-
-
+        /// <summary>
+        ///     Adds specified char to CLI input buffer.
+        /// </summary>
+        /// <param name="ch"></param>
         private void AddToBuffer(char ch)
         {
             if (ch >= 32 && ch <= 126)
                 _buffer.Append(ch);
         }
 
+        /// <summary>
+        ///     Removes last available char from CLI input buffer.
+        /// </summary>
         private void RemoveLastFromBuffer()
         {
             if (_buffer.Length == 0)
@@ -160,7 +174,10 @@ namespace SmartCLI
             _buffer.Remove(_buffer.Length - 1, 1);
         }
 
-
+        /// <summary>
+        ///     Updates state of machine depending on current CLI input buffer content. 
+        ///     This method is called internally in case of BACKSPACE or any symbol entered.
+        /// </summary>
         private void UpdateState()
         {
             string wildcard = GetCurrentWildcard();
@@ -212,7 +229,105 @@ namespace SmartCLI
             }
         }
 
+        /// <summary>
+        ///     Accepts current prompt (if any).
+        ///     This method is called internaly in case of TAB pressed.
+        /// </summary>
+        private void AcceptPrompt()
+        {
+            if (_prompt != string.Empty)                           
+                _buffer.Append(_prompt);            
+        }
 
+        /// <summary>
+        ///     Changes current token start position in CLI buffer. 
+        ///     This method is called internaly in case of SPACE pressed.
+        /// </summary>
+        private void GoToNextToken()
+        {
+            _unitGuess = null;
+            _prompt = string.Empty;
+            _wildcard = string.Empty;
+            _tokenStartPosition = _buffer.Length;
+        }
+
+        /// <summary>
+        ///     Switches current prompt to the next one (if any). 
+        ///     This method is called internaly in case of DOWN_ARROW pressed.     
+        /// </summary>
+        private void GetNextPrompt()
+        {
+            if (_unitsFound.Count <= 1)
+                return;
+            var next = _unitsFound.GetNext();
+            _unitGuess = next;
+            _prompt = next.Name[_wildcard.Length..];
+        }
+
+        /// <summary>
+        ///     Switches current prompt to the previous one (if any). 
+        ///     This method is called internaly in case of UP_ARROW pressed.     
+        /// </summary>
+        private void GetPreviousPrompt()
+        {
+            if (_unitsFound.Count <= 1)
+                return;
+            var prev = _unitsFound.GetPrevious();
+            _unitGuess = prev;
+            _prompt = prev.Name[_wildcard.Length..];
+        }
+
+        /// <summary>
+        ///     Stops machine's <see cref="ConsoleKeyInfo"/> processing procedure.
+        ///     This method is called internaly in case of ESCAPE pressed.  
+        /// </summary>
+        private void StopMachine()
+        {
+            _state = _state == State.Completed 
+                ? State.CancellationRequested 
+                : State.InputAborted;
+        }
+
+        /// <summary>
+        ///     Stops machine's <see cref="ConsoleKeyInfo"/> processing procedure.
+        ///     This method is called internaly in case of ENTER pressed.  
+        /// </summary>
+        private void Complete()
+        {
+            _state = State.Completed;
+        }
+
+        /// <summary>
+        ///     Registers all available commands in CLI environment for futher access.
+        /// </summary>
+        /// <param name="units">Collection of <see cref="ICliUnit"/>s.</param>
+        private void RegisterCommandHierarchy(IEnumerable<ICliUnit> units)
+        {
+            _searchEngine.RegisterUnitCollection(units);
+            foreach (var unit in units)
+                if (unit.SubUnits.Any())
+                    RegisterCommandHierarchy(unit.SubUnits);
+        }
+
+        /// <summary>
+        ///     Gets current wildcard according to CLI input buffer contents.
+        /// </summary>
+        /// <returns></returns>
+        private string GetCurrentWildcard()
+        {
+            int startIndex = _tokenStartPosition;
+            int length = _buffer.Length - _tokenStartPosition;
+            return _buffer.ToString(startIndex, length);
+        }
+
+        /// <summary>
+        ///     Tries to define <see cref="ICliUnit"/> in specified <see cref="ICliUnit"/> collection 
+        ///     according to specified wildcard.
+        /// </summary>
+        /// <param name="wildcard"></param>
+        /// <param name="units">Collection of <see cref="ICliUnit"/> from which unit should be defined.</param>
+        /// <param name="unitDefined">Pointer to <see cref="ICliUnit"/> instance for unit defined.</param>
+        /// <returns>TRUE if <see cref="ICliUnit"/> was successfully defined, otherwise - FALSE</returns>
         private bool TryDefineUnit(string wildcard, IEnumerable<ICliUnit> units, out ICliUnit? unitDefined)
         {
             _unitsFound.Clear();
@@ -229,79 +344,16 @@ namespace SmartCLI
             }
         }
 
-
+        /// <summary>
+        ///     Defines prompt according to specified wildcard.
+        /// </summary>
+        /// <param name="wildcard"></param>
+        /// <returns></returns>
         private string DefinePrompt(string wildcard)
         {
             ICliUnit currentGuess = _unitsFound.GetCurrent();
             return currentGuess.Name[wildcard.Length..];
         }
-
-
-
-        private void AcceptPrompt()
-        {
-            if (_prompt != string.Empty)                           
-                _buffer.Append(_prompt);            
-        }
-
-
-        private void GoToNextToken()
-        {
-            _unitGuess = null;
-            _prompt = string.Empty;
-            _wildcard = string.Empty;
-            _tokenStartPosition = _buffer.Length;
-        }
-        
-        private void GetNextPrompt()
-        {
-            if (_unitsFound.Count <= 1)
-                return;
-            var next = _unitsFound.GetNext();
-            _unitGuess = next;
-            _prompt = next.Name[_wildcard.Length..];
-        }
-
-        private void GetPreviousPrompt()
-        {
-            if (_unitsFound.Count <= 1)
-                return;
-            var prev = _unitsFound.GetPrevious();
-            _unitGuess = prev;
-            _prompt = prev.Name[_wildcard.Length..];
-        }
-
-        private void StopMachine()
-        {
-            _state = _state == State.Completed 
-                ? State.CancellationRequested 
-                : State.InputAborted;
-        }
-
-        private void Complete()
-        {
-            _state = State.Completed;
-        }
-
-
-        private void RegisterCommandHierarchy(IEnumerable<ICliUnit> units)
-        {
-            _searchEngine.RegisterUnitCollection(units);
-            foreach (var unit in units)
-                if (unit.SubUnits.Any())
-                    RegisterCommandHierarchy(unit.SubUnits);
-        }
-
-        private string GetCurrentWildcard()
-        {
-            int startIndex = _tokenStartPosition;
-            int length = _buffer.Length - _tokenStartPosition;
-            return _buffer.ToString(startIndex, length);
-        }
-
-
-
-        
     }
 
 }
