66f048f954861094264b8d174b98a08bf9fbe40b
kinest22 <nirvana897@gmail.com>
2024-07-16 00:42:20 +0300

    CliExecutionContext added

---
+++ src/CliExecutionContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCLI
+{
+    internal readonly struct CliExecutionContext
+    {
+        public ICliUnit CommandSpace { get; init; }
+        public ICliUnit Command { get; init; }
+        public IEnumerable<string> Arguments { get; init; }
+        public IReadOnlyDictionary<ICliUnit, string> Options { get; init; }
+    }
+}
